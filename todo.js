94b54b4128330b7f86f2a07ff5abe193a711ceb6
Nehal Bobhate <142580510+NehalBobhate@users.noreply.github.com>
2023-10-31 00:16:55 +0530

    To-Do-List
    Created To-Do-List Software. To log my daily to do task in a software.

---
+++ todo.js
@@ -0,0 +1,48 @@
+const taskEntry=document.getElementById("tskEntry");
+const taskList=document.getElementById("task-li");
+
+function addTask() 
+{
+	if(taskEntry.value=="") 
+	{
+		alert("Task cannot be Empty!");
+	}
+	else
+	{
+		let li=document.createElement("li");
+		li.innerHTML=taskEntry.value;
+		taskList.appendChild(li);
+		let span=document.createElement("span");
+		span.innerHTML="&#10060;";
+		li.appendChild(span);
+		saveData();		
+	}
+	taskEntry.value="";
+}
+
+taskList.addEventListener(
+	"click",
+	function (event) 
+	{
+		if (event.target.tagName==="LI") 
+		{
+			event.target.classList.toggle("checked");
+			saveData();
+		}
+		else if(event.target.tagName==="SPAN"){
+				event.target.parentElement.remove();
+				saveData();
+			}
+			console.log(event);
+		},
+		false
+);
+function saveData(){
+	localStorage.setItem("myTask",taskList.innerHTML);
+
+}
+function displayData()
+ {
+ 	taskList.innerHTML=localStorage.getItem("myTask");
+}
+displayData();
